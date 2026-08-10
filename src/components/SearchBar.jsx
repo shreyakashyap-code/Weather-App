015a37c83cae655b92d2b670c28d5279c4f59242
Shreya <vedansh0650@gmail.com>
2026-08-10 12:28:22 +0530

    make app fully responsive & change logo

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,35 +4,27 @@ function SearchBar({
   handleSearch,
 }) {
   return (
-    <div className="search-container">
+    <div className="search-box">
 
-      <div className="search-box">
+      <span className="search-icon">
+        🔍
+      </span>
 
-        <span className="search-icon">
-          🔍
-        </span>
-
-        <input
-          type="text"
-          placeholder="Enter city name..."
-          value={city}
-          onChange={(e) =>
-            setCity(e.target.value)
+      <input
+        type="text"
+        placeholder="Enter city name..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSearch();
           }
-          onKeyDown={(e) => {
+        }}
+      />
 
-            if (e.key === "Enter") {
-              handleSearch();
-            }
-
-          }}
-        />
-
-        <button onClick={handleSearch}>
-          Search
-        </button>
-
-      </div>
+      <button onClick={handleSearch}>
+        Search
+      </button>
 
     </div>
   );
